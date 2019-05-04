fe73f1e4d35b35a911a95f7744a61f4de3e046cf
unknown <lequangdn91@gmail.com>
2019-05-04 17:28:57 +0700

    nop bai tap

--- session17/js/form.js
+++ session17/js/form.js
@@ -1,16 +1,77 @@
 $(document).ready(function() {
-        $("#formDemo").validate({
-            rules: {
-                fullname : {
-                	required : true,
-                },
-                adress : "required",
-                phonenumber : "required"
+    $("#formDemo").validate({
+        rules: {
+            fullname : "required",
+            adress : "required",
+            phonenumber : "required",
+            startday : "required",
+            endday : "required",
+            firstcount : "required",
+            lastcount : "required",
+        },
+        messages: {
+            fullname: "*Fullname is required",
+            adress: "*Adress is required",
+            phonenumber : "*Phone number is required",
+            startday : "*Firstday is required",
+            endday : {
+                required : "*Lastday is required",                
             },
-            messages: {
-                fullname: "nhap ho ten",
-                adress: "nhap dia chi",
-                phonenumber : "nhap so dien thoai"
+            firstcount : "*Previous number is required",
+            lastcount : {
+                required : "*Current number is required",
+            },
+        },
+        submitHandler: function (form) {
+            $("#formDemo").hide();
+            $("#myform").show();
+            
+            var name = $(".name").val();
+            $("#myform").append("HO VA TEN:"+name+"<br>");
+            
+            var adress = $(".adr").val();
+            $("#myform").append("DIA CHI  :"+adress+"<br>");
+            
+            var phonenum = $(".phonenum").val();
+            $("#myform").append("SO DIEN THOAI  :"+phonenum+"<br>");
+            
+            var startdate = $(".startdate").val();
+            var enddate = $(".enddate").val();
+            $("#myform").append("NGAY DAU KY  :"+startdate+"<br>");
+            $("#myform").append("NGAY CUOI KY  :"+enddate+"<br>");
+
+            var previousnumber = $(".previousnumber").val();
+            var currentnumber = $(".currentnumber").val();
+            $("#myform").append("SO DAU KY  :"+previousnumber+"<br>");
+            $("#myform").append("SO CUOI KY  :"+currentnumber+"<br>");
+
+            var note = $(".Note").val();
+            $("#myform").append("GHI CHU  :"+note+"<br>");
+
+            var totalnumber = currentnumber - previousnumber;
+            var money;
+            if (totalnumber < 100) {
+                money = totalnumber*1500;
+            } else if ((totalnumber > 100) && (totalnumber < 200)) {
+                money = 100*1500 + (totalnumber-100)*2000;
+            } else {
+                money = 100*1500 + 100*2000 + (totalnumber-200)*3000;
             }
-        })
-    });
+            $("#myform").append("SO TIEN PHAI NOP  :"+money+"<br>");            
+        }
+    })
+
+    // compare date
+    $.validator.addMethod("enddate", function() {
+        var startdatevalue = $(".startdate").val();
+        var enddatevalue = $(".enddate").val();
+        return Date.parse(startdatevalue) <= Date.parse(enddatevalue);
+    }, "*End Date should be greater than Start Date");
+
+    // compare number
+    $.validator.addMethod("currentnumber", function() {
+        var previousnumbervalue = $(".previousnumber").val();
+        var currentnumbervalue = $(".currentnumber").val();
+        return previousnumbervalue <= currentnumbervalue;
+    }, "*Current number should be greater than Previous number");
+});
